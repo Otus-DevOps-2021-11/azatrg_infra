fcd78a0cddd7bf936aee7a761eab7e149ac0c89a
azatrg <azat.r.garaev@ya.ru>
2021-12-30 12:58:33 +0300

    1. Создал файл main.tf и инициализировал провайдера.

---
+++ terraform/main.tf
@@ -0,0 +1,6 @@
+provider "yandex" {
+  token     = "AQAAAABX-5zoAATuwb9Q0WRMN0WJiFnfWMJbCMA"
+  cloud_id  = "b1geeigb8tm4mbhthlqf"
+  folder_id = "b1g6rdsrvhrut8j2904c"
+  zone      = "ru-central1-a"
+}

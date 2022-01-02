69ce62aaf4845904e3a944db950e9d17f93b385f
azatrg <azat.r.garaev@ya.ru>
2022-01-02 20:02:56 +0300

    Обновил README.md

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -5,7 +5,11 @@ resource "yandex_lb_target_group" "lb-tg" {
 
   target {
     subnet_id = var.subnet_id
-    address   = "${yandex_compute_instance.app[0].network_interface.0.ip_address}"
+    address   = "${yandex_compute_instance.app1.network_interface.0.ip_address}"
+  }
+    target {
+    subnet_id = var.subnet_id
+    address   = "${yandex_compute_instance.app2.network_interface.0.ip_address}"
   }
 }
 
--- terraform/main.tf
+++ terraform/main.tf
@@ -5,11 +5,10 @@ provider "yandex" {
   zone                     = var.zone
 }
 
-resource "yandex_compute_instance" "app" {
-  name        = "${var.instance-name}-${count.index + 1}"
+resource "yandex_compute_instance" "app1" {
+  name        = "reddit-app-1"
   platform_id = "standard-v1"
   zone        = var.zone
-  count = 1
 
   resources {
     cores  = 2
@@ -36,7 +35,54 @@ resource "yandex_compute_instance" "app" {
 
   connection {
     type  = "ssh"
-    host  = yandex_compute_instance.app[0].network_interface.0.nat_ip_address
+    host  = yandex_compute_instance.app1.network_interface.0.nat_ip_address
+    user  = "ubuntu"
+    agent = false
+    # путь до приватного ключа
+    private_key = file(var.private_key_path)
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+}
+
+resource "yandex_compute_instance" "app2" {
+  name        = "reddit-app-2"
+  platform_id = "standard-v1"
+  zone        = var.zone
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      # Указать id образа созданного в предыдущем домашем задании
+      image_id = var.image_id
+      type     = "network-ssd"
+    }
+  }
+
+  network_interface {
+    # Указан id подсети default-ru-central1-a
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type  = "ssh"
+    host  = yandex_compute_instance.app2.network_interface.0.nat_ip_address
     user  = "ubuntu"
     agent = false
     # путь до приватного ключа

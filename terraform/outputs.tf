da7ca7c36572c2c8408011cba33ff92a956c18f0
azatrg <azat.r.garaev@ya.ru>
2022-01-02 16:46:50 +0300

    Добавил в output адрес балансировщика.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,5 +2,5 @@ output "external_ip_address_app" {
   value = yandex_compute_instance.app[0].network_interface.0.nat_ip_address
 }
 output "nlb_listener_ip_address" {
-  value = yandex_lb_network_load_balancer.nlb.listener[*].external_address_spec.*.address
+  value = flatten(yandex_lb_network_load_balancer.nlb.listener[*].external_address_spec.*.address)[0]
 }

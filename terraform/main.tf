fdfcab11557bf78914cd3dd686615d3532f2789c
azatrg <azat.r.garaev@ya.ru>
2022-01-12 12:17:24 +0300

    1. Вынес описание инстансов app и db в соответствующие модули. 2. внес правки в основные main.tf и outputs.tf для вызова модулей.

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,20 @@ provider "yandex" {
   zone                     = var.zone
 }
 
+module "app" {
+  source          = "./modules/app"
+  public_key_path = var.public_key_path
+  app_disk_image  = var.app_disk_image
+  subnet_id       = var.subnet_id
+}
+
+module "db" {
+  source          = "./modules/db"
+  public_key_path = var.public_key_path
+  db_disk_image   = var.db_disk_image
+  subnet_id       = var.subnet_id
+}
+
 # resource "yandex_compute_instance" "app" {
 #   name        = "${var.instance_name}-${count.index + 1}"
 #   platform_id = "standard-v1"

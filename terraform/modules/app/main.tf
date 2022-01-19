00044b0ee02155f5f8aafc39377eada290f07703
azatrg <azat.r.garaev@ya.ru>
2022-01-19 19:24:48 +0300

    1. Отключил provision в terraform. 2. Добавил плейбук reddit_app.yml - меняет bind ip у mongod

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -36,12 +36,12 @@ resource "yandex_compute_instance" "app" {
     private_key = file(var.private_key_path)
   }
 
-  provisioner "file" {
-    content     = templatefile("${path.module}/files/puma.service.tmpl", { DATABASE_URL = var.database_url })
-    destination = "/tmp/puma.service"
-  }
+  # provisioner "file" {
+  #   content     = templatefile("${path.module}/files/puma.service.tmpl", { DATABASE_URL = var.database_url })
+  #   destination = "/tmp/puma.service"
+  # }
 
-  provisioner "remote-exec" {
-    script = "${path.module}/files/db_url.sh"
-  }
+  # provisioner "remote-exec" {
+  #   script = "${path.module}/files/db_url.sh"
+  # }
 }

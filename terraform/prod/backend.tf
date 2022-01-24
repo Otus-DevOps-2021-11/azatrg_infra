d17347fb443ed03cfae7ef1ec1eccffd252f6734
azatrg <azat.r.garaev@ya.ru>
2022-01-24 11:21:28 +0300

    1. Добавил плейбук с созданием пользователей. 2. пароли зашифровал с помощью ansible vault

--- terraform/prod/backend.tf
+++ terraform/prod/backend.tf
@@ -2,11 +2,11 @@ terraform {
 
   backend "s3" {
     endpoint   = "storage.yandexcloud.net"
-    bucket     = "azatrg-s3-tf-states"
+    bucket     = "azatrg-otus-infra"
     region     = "us-east-1"
     key        = "terraform/prod/state/terraform.tfstate"
-    access_key = "QS9Msdfdfsdfsdf23423"
-    secret_key = "pT1eoJxvvjVMnfsdfdsf232323"
+    access_key = "QS9M-8FAPgoHvG7j6owi"
+    secret_key = "pT1eoJxvvjVMn7KCh2uPmRIqoQyUviNEzgH7y1L8"
 
     skip_region_validation      = true
     skip_credentials_validation = true
--- terraform/stage/variables.tf
+++ terraform/stage/variables.tf
@@ -6,49 +6,37 @@ variable folder_id {
 }
 variable zone {
   description = "Zone"
-  # Значение по умолчанию
   default = "ru-central1-a"
 }
 variable network_name {
   description = "network,subnet name"
   default     = "reddit-net-stage"
 }
-variable instance_name {
-  description = "Instance name"
-}
 variable app_instance_name {
   description = "Instance name"
-  default     = "reddit-app-stage"
+  default     = "reddit-app"
 }
 variable db_instance_name {
   description = "Instance name"
-  default     = "reddit-db-stage"
+  default     = "reddit-db"
 }
 variable instance_count {
   description = "Number of instances"
   default     = 1
 }
 variable public_key_path {
-  # Описание переменной
   description = "Path to the public key used for ssh access"
 }
 variable private_key_path {
-  # Описание переменной
   description = "Path to the public key used for ssh access"
-}
-variable image_id {
-  description = "Disk image"
 }
 variable app_disk_image {
   description = "Disk image for reddit app"
-  default     = "reddit-app-base"
+  # default     = "reddit-app-base"
 }
 variable db_disk_image {
   description = "Disk image for reddit db"
-  default     = "reddit-db-base"
-}
-variable subnet_id {
-  description = "Subnet"
+  # default     = "reddit-db-base"
 }
 variable service_account_key_file {
   description = "key .json"
